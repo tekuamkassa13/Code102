4891341b80cb2468530b72574afe0b165ab58c9b
tekuamkassa13 <Tekuam.Kassa@gmail.com>
2019-02-15 20:49:39 -0500

    js file updated

---
+++ main.js
@@ -0,0 +1,17 @@
+//let name = tekuam;
+//let age = 24;
+
+//if(age>20){
+   // alert(" Your are an adult")
+//} else{
+  //  alert(" You need to grow up!")
+//}
+
+let speedLimit =35;
+let currentSpeed =45;
+
+if(currentSpeed>speedLimit){
+    alert("Take a Picture")
+}else{
+    alert("Keep it up")
+}
